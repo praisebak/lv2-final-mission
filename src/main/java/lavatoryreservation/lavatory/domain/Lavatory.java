661b187df9391d92dae58b537088f90e6b76e4cd
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-06-10 14:11:34 +0900

    feat: 화장실 구현

--- src/main/java/lavatoryreservation/lavatory/domain/Lavatory.java
+++ src/main/java/lavatoryreservation/lavatory/domain/Lavatory.java
@@ -1,4 +1,34 @@
 package lavatoryreservation.lavatory.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Lavatory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Sex sex;
+    private String description;
+
+    public Lavatory(Long id, Sex sex, String description) {
+        this.id = id;
+        this.sex = sex;
+        this.description = description;
+    }
+
+    protected Lavatory() {
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
